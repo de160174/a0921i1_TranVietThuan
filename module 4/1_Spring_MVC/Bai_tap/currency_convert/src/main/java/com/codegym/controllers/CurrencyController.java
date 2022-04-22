2cbc50aa7eaced1d019cd86d0e7e1cad4dd25c73
de160174 <81423827+de160174@users.noreply.github.com>
2022-04-22 21:15:25 +0700

    nop bai

--- module 4/1_Spring_MVC/Bai_tap/currency_convert/src/main/java/com/codegym/controllers/CurrencyController.java
+++ module 4/1_Spring_MVC/Bai_tap/currency_convert/src/main/java/com/codegym/controllers/CurrencyController.java
@@ -3,11 +3,13 @@ package com.codegym.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CurrencyController {
-    @GetMapping("/convert")
+    @RequestMapping(value = "/convert", method = RequestMethod.GET)
     public String convert(@RequestParam String usd, Model model) {
         double vnd = Double.parseDouble(usd) * 23000;
         model.addAttribute("vnd", vnd);
