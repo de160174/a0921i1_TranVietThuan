698abf0531e964c48950985f45b3e0981a83dec7
de160174 <81423827+de160174@users.noreply.github.com>
2022-05-16 08:39:45 +0700

    nop bai

---
+++ module 4/7_spring_data_repository/src/main/java/com/codegym/blog/repositories/BlogRepositories.java
@@ -0,0 +1,15 @@
+package com.codegym.blog.repositories;
+
+import com.codegym.blog.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BlogRepositories extends JpaRepository<Blog, Integer> {
+    @Query(value = "select * from blog where header like %:c%", nativeQuery = true)
+    Page<Blog> searchByCharacter(@Param("c") String c, Pageable p);
+
+    Page<Blog> findBlogByCategory_Id(Integer id, Pageable p);
+}
